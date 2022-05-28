e216f01fcea4f67c9fd3b4edd83b19e067b007fd
debbyeze <deborahsong7@gmail.com>
2022-05-28 15:41:56 +0100

    3rd commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const nav = document.getElementsByTagName("nav");
+const navItem = document.querySelector(".nav-item");
+const list = document.querySelector(".nav-list");
+var hamburger = document.getElementById("ham");
+
+function myHamburger() {
+    // nav.style.display="inline-block";
+    // navItem.style.display="block";
+    hamburger.style.border="20px";
+
+}
